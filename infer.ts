2e60dc7113f26dfe7127c5cf0ee1792c0292b84a
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2025-05-31 15:59:19 +0100

    use a ReturnType

--- infer.ts
+++ infer.ts
@@ -5,4 +5,5 @@ function add(a: number, b: number) {
 type AddFn = typeof add
 type ReturnValueType<T> = T extends (...args: any[]) => infer RV ? RV : never
 
-type AddFnReturnValueType = ReturnValueType<AddFn>
+// type AddFnReturnValueType = ReturnValueType<AddFn>
+type AddFnReturnValueType = ReturnType<AddFn>
